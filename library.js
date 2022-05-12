23dd09f0e8a9479871c60a93e6e1c66cb5c652f1
David Ho <davidho0403@gmail.com>
2022-05-12 17:27:36 +0800

    feat: getTime() -> library
    Add getTime() function

---
+++ library.js
@@ -0,0 +1,9 @@
+const getTime = () => {
+    let time = new Date();
+    date = time.getFullYear() + "-" + (time.getMonth() + 1) + "-" + time.getDate();
+    let hour = time.getHours().toString().padStart(2, '0');
+    let minute = time.getMinutes().toString().padStart(2, '0');
+    let second = time.getSeconds().toString().padStart(2, '0');
+
+    return (date + " " + hour + ":" + minute + ":" + second);
+};
